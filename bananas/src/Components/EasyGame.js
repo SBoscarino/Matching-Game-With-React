c38ac79b19d94acef923c214fea6570a8d1ffaab
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2018-06-12 19:39:17 -0500

    about to start the card flippin

--- bananas/src/Components/EasyGame.js
+++ bananas/src/Components/EasyGame.js
@@ -7,18 +7,28 @@ class EasyGame extends Component {
     super(props)
     this.state = {
       isToggleOnEasy: this.props.isToggleOnEasy,
-      cards: 20,
+      cards: 10,
       cardDeck: []
     }
   }
 
+tellMe(){
+  console.log(this.state.cardDeck)
+}
   //This component only renders if the Easy Mode is active.
   render() {
-    console.log("props in EasyGame:", this.props);
-    console.log("state in EasyGame:", this.state);
+    //console.log("props in EasyGame:", this.props);
+    //console.log("state in EasyGame:", this.state);
+    let arrayOfCardBacks = ['p', 'i', 'k', 'a', 'c', 'p', 'i', 'k', 'a', 'c'];
     if (this.state.isToggleOnEasy === true){
       for (let i = 0; i < this.state.cards; i++){
-       this.state.cardDeck.push(<img className="EasyCardImg" alt="Card" src={aSingleCard}/>)
+       this.state.cardDeck.push(
+          {frontImg:<img className="EasyCardImg" alt="Card" src={aSingleCard}/>,
+          isFlipped: false, 
+          backImg:arrayOfCardBacks[i], 
+          cardNumber: i
+          }
+       )
       };
     }
   
@@ -27,7 +37,7 @@ class EasyGame extends Component {
           <div className="cardContainer">
           {this.state.cardDeck.map((item, i) => {
             return(
-              <li className="easyCardsLI" key={i}>{item}</li>
+              <li className="easyCardsLI" onClick={(e) => this.tellMe(e)} key={i}>{item.frontImg}</li>
             )})
           }
           </div>
